6aacfdeb65b87669a3a764c924b6d97496f2fc2a
Caio <devcaiosantos@gmail.com>
2021-08-17 13:52:57 -0300

    retirada box initialChat

--- src/components/chatContainer/InitialChat.js
+++ src/components/chatContainer/InitialChat.js
@@ -4,9 +4,7 @@ export const InitialChat = ({active, serverChat,urlHost}) => {
     
     return(
         <Box  height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-            <Box borderTop={"2px"} borderBottom={"2px"} p={2} bg={"#FFFFFF"} display={"flex"} justifyContent={"center"} width={"100%"}>
             <img width={"200px"} src={`${urlHost}/mactus.png`}/>
-            </Box>
         </Box>
     )
 }
